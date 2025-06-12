3bf3c1e48806a91f1505ecca08d951371b36c31e
Kshitij Bajracharya <bkshitij.dev@gmail.com>
2025-06-12 15:34:14 +0545

    Add initial files

---
+++ src/main/java/com/kshitij/bookflipster/BookflipsterApplication.java
@@ -0,0 +1,13 @@
+package com.kshitij.bookflipster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookflipsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookflipsterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kshitij/bookflipster/BookflipsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kshitij.bookflipster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookflipsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
